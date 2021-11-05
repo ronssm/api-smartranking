29046151745ece0366b64cfa9ce577d79971b301
Ronaldo <ronaldo.kamus@gmail.com>
2021-11-04 22:22:56 -0300

    build: add docker configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { PlayersModule } from './players/players.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ChallengesModule } from './challenges/challenges.module';
+import { PlayersModule } from './players/players.module';
 import { RankingsModule } from './rankings/rankings.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-    MongooseModule.forRoot('', {
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
-    ),
+    MongooseModule.forRoot('mongodb://localhost:27017/apismartranking', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
     PlayersModule,
     CategoriesModule,
     ChallengesModule,
